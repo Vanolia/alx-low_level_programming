cb184966df270bd0f7a865e15d1c32e7b26b30fe
Vanolia <reddyvanolia21@gmail.com>
2023-02-21 01:41:52 -0800

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('w');
+	_putchar('t');
+	_putchar('c');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
