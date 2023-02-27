6fd39efbea79e3abfdb3be9d861e34a3c2810976
Vanolia <reddyvanolia21@gmail.com>
2023-02-27 02:06:13 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,21 @@
 #include "main.h"
+
+
 /**
  *_strlen - returns the length of a string
  *@a: string
  *Return: length
  */
-int strlen (char *s)
+int _strlen(char *s)
 {
-	int longi = 0;
+	int len = 0;
+
 
 	while (*s != '\0')
 	{
-		longi++;
+		len++;
 		s++;
 	}
 
-	return (longi);
+	return (len);
 }
