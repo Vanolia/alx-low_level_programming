4e72ec0f923b56f3b960bfc7ff632176bc307b26
Vanolia <reddyvanolia21@gmail.com>
2023-03-07 23:32:05 -0800

    2-strlen_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,13 +12,13 @@
  *
  * Return: The value of x raised to the power of y.
  */
+
 int _pow_recursion(int x, int y)
 {
-	int result = x;
+	int = x;
 
 	if (y < 0)
 		return (-1);
 
 	else if (y == 0)
 		return (1);
-}
